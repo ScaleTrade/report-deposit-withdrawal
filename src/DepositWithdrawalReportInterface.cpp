3ff8a12c49acfa8ffd26b8e4f9b8aecc299ae304
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-11-19 08:36:08 +0300

    Work in progress

--- src/DepositWithdrawalReportInterface.cpp
+++ src/DepositWithdrawalReportInterface.cpp
@@ -14,7 +14,7 @@ extern "C" void AboutReport(rapidjson::Value& request,
         Value().SetString("Balance operations performed on trading accounts over the selected period. "
                           "Includes transaction ID, date, amount, and account details.",
              allocator), allocator);
-    response.AddMember("type", REPORT_GROUP_TYPE, allocator);
+    response.AddMember("type", REPORT_RANGE_GROUP_TYPE, allocator);
 }
 
 extern "C" void DestroyReport() {}
@@ -24,18 +24,31 @@ extern "C" void CreateReport(rapidjson::Value& request,
                              rapidjson::Document::AllocatorType& allocator,
                              CServerInterface* server) {
     // Структура накопления итогов
-    // struct Total {};
-    //
-    // std::unordered_map<std::string, Total> totals_map;
+    struct Total {
+        double balance;
+        std::string currency;
+    };
+
+    std::unordered_map<std::string, Total> totals_map;
 
     std::string group_mask;
-    if (request.HasMember("group") && request["group"].IsString()) {
+    int from;
+    int to;
+    if (request.HasMember("group") && request["group"].IsNumber()) {
         group_mask = request["group"].GetString();
     }
+    if (request.HasMember("from") && request["from"].IsNumber()) {
+        from = request["from"].GetInt();
+    }
+    if (request.HasMember("to") && request["to"].IsString()) {
+        to = request["to"].GetInt();
+    }
 
+    std::vector<TradeRecord> trades_vector;
 
     try {
-
+        server->GetTransactionsByGroup(group_mask, from, to, &trades_vector);
+        std::cout << "Trades vector size: " << trades_vector.size() << std::endl;
     } catch (const std::exception& e) {
         std::cout << "[DepositWithdrawalReportInterface]: " << e.what() << std::endl;
     }
