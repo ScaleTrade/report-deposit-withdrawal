b285754ed3a7d23768aee09a0132023aa18e1af4
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-11-24 15:07:28 +0300

    Work in progress

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 #include "ast/Ast.hpp"
 #include "Structures.hpp"
 
@@ -7,4 +11,6 @@ namespace utils {
     void CreateUI(const ast::Node& node,
               rapidjson::Value& response,
               rapidjson::Document::AllocatorType& allocator);
+
+    std::string FormatTimestampToString(const time_t timestamp);
 }
