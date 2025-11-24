56d69f3e8fb7af811f750333b5d4ad249b43272e
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-11-24 12:23:20 +0300

    Work in progress

--- src/PluginInterface.cpp
+++ src/PluginInterface.cpp
@@ -95,6 +95,15 @@ extern "C" void CreateReport(rapidjson::Value& request,
         }));
 
         for (const auto& trade : trades_vector) {
+            tbody_rows.push_back(tr({
+                td({div({text(std::to_string(trade.order))})}),
+                td({div({text(std::to_string(trade.login))})}),
+                td({div({text("NAME")})}),
+                td({div({text(std::to_string(trade.timestamp))})}),
+                td({div({text(trade.comment)})}),
+                td({div({text(format_for_AST(trade.profit))})}),
+                td({div({text("CURRENCY")})}),
+            }));
             // if (trade.cmd == OP_BALANCE_IN || trade.cmd == OP_BALANCE_OUT) {
             //     tbody_rows.push_back(tr({
             //         td({div({text(std::to_string(trade.order))})}),
@@ -106,15 +115,6 @@ extern "C" void CreateReport(rapidjson::Value& request,
             //         td({div({text("CURRENCY")})}),
             //     }));
             // }
-            tbody_rows.push_back(tr({
-                td({div({text(std::to_string(trade.order))})}),
-                td({div({text(std::to_string(trade.login))})}),
-                td({div({text("NAME")})}),
-                td({div({text(std::to_string(trade.timestamp))})}),
-                td({div({text(trade.comment)})}),
-                td({div({text(format_for_AST(trade.profit))})}),
-                td({div({text("CURRENCY")})}),
-            }));
         }
 
         return table({
