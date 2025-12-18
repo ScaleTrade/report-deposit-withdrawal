c13677976ff2b3fbb7a63154d75d01cc552e3cfa
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-12-18 10:08:03 +0300

    Upd

--- src/PluginInterface.cpp
+++ src/PluginInterface.cpp
@@ -118,4 +118,6 @@ extern "C" void CreateReport(rapidjson::Value& request,
     });
 
     utils::CreateUI(report, response, allocator);
+
+    std::cout << "[DepositWithdrawalReportInterface]: Data sent" << std::endl;
 }
